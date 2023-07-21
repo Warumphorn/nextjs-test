133122e90d2b5172b3e9ff99d4f5eccd2309f36b
Warumphorn <Moofah.Warumphorn@gmail.com>
2023-07-21 14:19:09 +0700

    index

--- pages/ken/index.js
+++ pages/ken/index.js
@@ -2,13 +2,8 @@
 export default function Ken(props) {
     return (
         <div>
-<<<<<<< HEAD
-        <div>{props.text} - {props.testset} hello world</div>
-        <div>TEST</div>
-=======
         <div>{props.text} - {props.testset}</div>
         <div> Warumphorn </div>
->>>>>>> 2ca82f1bad5e2cca7148cab9cb17ebbdd9808e3b
         </div>
     )
 }
