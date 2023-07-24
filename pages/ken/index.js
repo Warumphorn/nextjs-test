791c7e8b48ca169f4abffe50c9f9afddb5b1f4b8
Warumphorn <139833498+Warumphorn@users.noreply.github.com>
2023-07-24 15:50:50 +0700

    Update index.js

--- pages/ken/index.js
+++ pages/ken/index.js
@@ -5,6 +5,7 @@ export default function Ken(props) {
         <div>{props.text} - {props.testset}</div>
         <div> Warumphorn </div>
         <div> Test Dev  </div>
+         <div> Hello world  </div>
         </div>
     )
 }
